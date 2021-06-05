2619e3053ffff4826aede435d63263a972b88d3e
jude-co <atalobhor@gmail.com>
2021-06-05 13:38:40 +0100

    third commit

--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -4,18 +4,17 @@ export default function TodoList() {
     return (
         <div>
            <h1 className ='todo'>Todo App</h1> 
-           <input className="search" input name="q" type="search" title="Add your new todo" placeholder="Add your new todo"></input><button>+</button>
+            <input className="search" input name="q" type="search" placeholder="Add your new todo"></input><button className="add">+</button>
 
-           <div className ="list">
-                <ul>
-                      <span className ="buy"><li>Buy a new gaming laptop</li></span>
-                      <span className ="task"><li>Complete a previous task</li></span>
-                      <span className ="dance"><li>Create a dance video</li></span>
-                      <span className ="new"><li>Create a new portfolio site</li></span>
-                </ul>
+            <div className ="list">
+                
+             <span className ="buy">Create a new dance video</span>
+             <button className="edit">Edit</button>
+             <button className="delete">Delete</button>
+
            </div>
-           <span className ="clear">
-               <h2>You have 4 pending tasks</h2><button>Clear All</button>
+           <span className ="clearall">
+               <button className="clear">Clear All</button>
            </span>
            
         </div>
